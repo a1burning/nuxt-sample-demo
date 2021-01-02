c3b19eb2cb17156ae50c22b838e88ede01d26892
csf <shuangfeng1993@163.com>
2021-01-02 17:33:04 +0800

    05-嵌套路由

---
+++ pages/users.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Users</h1>
+    <!-- 子路由出口 -->
+    <nuxt-child />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UsersPage'
+}
+</script>
+
+<style></style>
